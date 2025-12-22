202ff76b584fe7124031477e0db20b959c7099ad
Mansur <vakhabov.mans@gmail.com>
2025-12-22 07:29:53 +0300

    add mutex for fix data race

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"github/m4nsur/concurrency-lrn/miner"
 	"github/m4nsur/concurrency-lrn/postman"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 func main() {
-	var coals int
+	var coals atomic.Int64
 	var mails []string
-
+	var mu sync.Mutex
 	contextMiner, cancelMiner := context.WithCancel(context.Background())
 	contextPostman, cancelPostman := context.WithCancel(context.Background())
 
@@ -28,31 +30,25 @@ func main() {
 	coalTransferPoint := miner.MinerPool(contextMiner, 10)
 	mailsTransferPoint := postman.PostmanPool(contextPostman, 10)
 
-	isCoalClosed := false 
-	isMailsClosed := false 
+	wg := sync.WaitGroup{}
 
-	for !isCoalClosed || !isMailsClosed {
-		select {
-		case coal, ok := <- coalTransferPoint:
-			if !ok {
-				isCoalClosed = true
-				continue
-			}
-
-			coals += coal
-
-		case letter, ok := <- mailsTransferPoint:
-			if !ok {
-				isMailsClosed = true
-				continue
-			}
-
-			mails = append(mails, letter)
+	wg.Go(func() {
+		for coal := range coalTransferPoint {
+			coals.Add(int64(coal))
 		}
+	})
 
-	}
+	wg.Go(func() {
+		for letter := range mailsTransferPoint {
+			mu.Lock()
+			mails = append(mails, letter)
+			mu.Unlock()
+		}
+	})	
 
-	fmt.Println("Coals:", coals)
+	wg.Wait()
+	
+	fmt.Println("Coals:", coals.Load())
 	fmt.Println("Mails:", len(mails))
 
 }
